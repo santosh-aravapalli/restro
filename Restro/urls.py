10cd2bc29cf3a095fc1b0755c7745065aee0b8fb
aravapalli <santosh.aravapalli@gmail.com>
2020-12-16 22:24:24 +0530

    modifed urls

--- Restro/urls.py
+++ Restro/urls.py
@@ -21,5 +21,6 @@ from django.urls import path,include
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('admin/',include('restro_admin.urls')),
+    path('api/',include('api.urls')),
 
 ]
